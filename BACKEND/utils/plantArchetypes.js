53a823fd310c1afb50d06cabb33049578de8f099
Komal <komalkerlekar1705@gmail.com>
2026-03-24 06:10:02 +0530

    update: expanded plant archetypes with realistic dataset

--- BACKEND/utils/plantArchetypes.js
+++ BACKEND/utils/plantArchetypes.js
@@ -38,34 +38,82 @@ const plantArchetypes = {
 
 const plantNameToArchetype = {
   // Succulents
-  "snake plant": "succulent",
   "aloe vera": "succulent",
   "jade plant": "succulent",
+  "haworthia": "succulent",
+  "echeveria": "succulent",
+  "kalanchoe": "succulent",
+  "zebra plant": "succulent",
+  "burro's tail": "succulent",
+  "string of pearls": "succulent",
+  "gasteria": "succulent",
 
   // Herbs
   "basil": "herb",
   "mint": "herb",
   "coriander": "herb",
+  "curry leaves": "herb",
+  "ajwain": "herb",
+  "parsley": "herb",
+  "oregano": "herb",
+  "thyme": "herb",
+  "lemongrass": "herb",
 
   // Foliage plants
   "money plant": "foliage",
   "philodendron": "foliage",
   "rubber plant": "foliage",
+  "pothos": "foliage",
+  "areca palm": "foliage",
+  "dracaena": "foliage",
+  "aglaonema": "foliage",
+  "dieffenbachia": "foliage",
+  "parlor palm": "foliage",
+  "calathea": "foliage",
+  "croton": "foliage",
 
   // Flowering plants
   "rose": "flowering",
+  "hibiscus": "flowering",
+  "jasmine": "flowering",
+  "mogra": "flowering",
+  "marigold": "flowering",
+  "chrysanthemum": "flowering",
+  "petunia": "flowering",
+  "zinnia": "flowering",
+  "vinca": "flowering",
+  "periwinkle": "flowering",
+  "balsam": "flowering",
+  "cosmos": "flowering",
+  "bougainvillea": "flowering",
+  "sunflower": "flowering",
+  "geranium": "flowering",
+  "dahlia": "flowering",
   "peace lily": "flowering",
+  "anthurium": "flowering",
   "orchid": "flowering",
+  "begonia": "flowering",
+  "impatiens": "flowering",
 
   // Low light plants
   "zz plant": "lowLight",
   "cast iron plant": "lowLight",
   "chinese evergreen": "lowLight",
+  "snake plant": "lowLight",
+  "aglaonema": "lowLight",
+  "dracaena": "lowLight",
+  "parlor palm": "lowLight",
+  "lucky bamboo": "lowLight",
+
 
   // Tropical plants
   "areca palm": "tropical",
   "monstera": "tropical",
-  "banana plant": "tropical"
+  "banana plant": "tropical",
+  "bird of paradise": "tropical",
+  "alocasia": "tropical",
+  "fern": "tropical",
+  "bamboo palm": "tropical",
 };
 
 module.exports = { plantArchetypes, plantNameToArchetype };
